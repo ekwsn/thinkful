414b68e913228851223a6b47d5719a4b123807e6
Lok <lokhim95@hotmail.com>
2015-08-26 15:27:14 +0800

    Lesson 2-5

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -18,9 +18,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        redSlider.value = defaults.floatForKey("red")
+        greenSlider.value = defaults.floatForKey("green")
+        blueSlider.value = defaults.floatForKey("blue")
+        
         updateBackgroundColor()
         colorSquare.layer.borderColor = UIColor.blackColor().CGColor
         colorSquare.layer.borderWidth = 1
+        
         
     }
 
@@ -36,7 +43,23 @@ class ViewController: UIViewController {
         let blue = CGFloat(blueSlider.value)
         
         colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setFloat(redSlider.value, forKey: "red")
+        defaults.setFloat(greenSlider.value, forKey: "green")
+        defaults.setFloat(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        if (segue.identifier == "openColor") {
+        
+        let newViewController = segue.destinationViewController as! UIViewController
+
+        newViewController.view.backgroundColor = colorSquare.backgroundColor
+
+    }
+    }
 }
 
